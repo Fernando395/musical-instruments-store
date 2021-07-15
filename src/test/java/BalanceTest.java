804e43cabfa20c681ebc560e8dac99b50c9214d8
Fernando395 <Fe395rodrigues@gmail.com>
2021-07-15 17:43:24 -0300

    resulvido problema de violação de responsabilidade única

--- src/test/java/BalanceTest.java
+++ src/test/java/BalanceTest.java
@@ -6,14 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BalanceTest {
 
     @Test
-    public void shouldAddMoneyToBalance() {
+    public void shouldThrowAnException() {
         Balance balance = new Balance();
         balance.addMoney(129.90);
-        assertEquals(129.90, balance.getMoney());
         balance.withdrawMoney(129.90);
         assertEquals(0, balance.getMoney());
         assertThrows(IllegalArgumentException.class, () -> {
             balance.withdrawMoney(50000.00);
         });
+    }
+
+    @Test
+    public void shouldAddMoneyToBalance() {
+        Balance balance = new Balance();
+        balance.addMoney(129.90);
+        assertEquals(129.90, balance.getMoney());
+    }
+
+    @Test
+    public void shouldDrawMoneyToBalance() {
+        Balance balance = new Balance();
+        balance.addMoney(129.90);
+        balance.withdrawMoney(129.90);
+        assertEquals(0, balance.getMoney());
     }
 }
